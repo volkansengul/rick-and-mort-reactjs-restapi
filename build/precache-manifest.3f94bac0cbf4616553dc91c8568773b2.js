04f490058f8fe8768a9e4d06f2e7c8d7c344554b
volkan <volkan.sengul@smartlimon.com>
2019-02-20 15:38:22 +0300

    app's build files added

---
+++ build/precache-manifest.3f94bac0cbf4616553dc91c8568773b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f04afd3816ee656a9578",
+    "url": "/static/js/main.f04afd38.chunk.js"
+  },
+  {
+    "revision": "50e728156ca5a199d5bd",
+    "url": "/static/js/2.50e72815.chunk.js"
+  },
+  {
+    "revision": "f04afd3816ee656a9578",
+    "url": "/static/css/main.03b498da.chunk.css"
+  },
+  {
+    "revision": "d71d404361c2d015aa6d403aa0563c7c",
+    "url": "/index.html"
+  }
+];
